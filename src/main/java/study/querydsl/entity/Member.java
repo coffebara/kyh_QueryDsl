174536a83994bee391eafbefc60c6b4ca861dc42
allready-dawn <qwshpke@naver.com>
2024-03-25 23:59:33 +0900

    예제 도메인 모델

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -1,18 +1,38 @@
 package study.querydsl.entity;
 
+
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
-    @Column(name = "memeber_id")
+    @Column(name = "member_id")
     private Long id;
     private String username;
     private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+       @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+//            changeTeam(team);
+        }
+    }
+
 
 
 }
